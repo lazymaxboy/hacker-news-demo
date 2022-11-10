ef6ae982b0ca457bf300157fd11f6789f52a44c7
trung <trungezeal115@gmail.com>
2022-11-10 21:01:00 +0700

    dsad

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./src",
   plugins: [react()],
-  publicDir: "assets",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-      },
-    },
-  },
 });
